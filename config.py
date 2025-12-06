c8935772f9b1b36e23941aa0c0db7f22310a8a0c
Kyescentia <87762092+Kyescentia@users.noreply.github.com>
2025-12-06 12:21:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "thisemployee.cvyidhwdve74.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "haHAHAHAHAHAHAhahahahahahaha#123456"
+customdb = "employee"
+custombucket = "some-employee-bunket"
+customregion = "us-east-1"
+
